cda8923fd0e9eda6b254504e923cecf4e4715caa
polrayo <pol.rayo.sanchez@es.ey.com>
2026-06-12 14:18:28 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,6 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// export default defineConfig({
-//   plugins: [react()],
-//   server: {
-//     port: 5173,
-//     host: true,
-//   },
-// })
-
-
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { viteSingleFile } from 'vite-plugin-singlefile'
-
-export default defineConfig({
-  plugins: [react(), viteSingleFile()],
-  base: './',
-  server: {
-    port: 5173,
-    host: true,
-  },
-})
-
-import { defineConfig } from 'vite';
-import fs from 'fs';
-import path from 'path';
+import fs from 'fs'
+import path from 'path'
 
 export default defineConfig({
   build: {
@@ -42,4 +17,4 @@ export default defineConfig({
       }
     }
   ]
-});
+})
