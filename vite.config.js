c479266e7eb1d7e62f7e24e4e1cdf19365b299b4
polrayo <pol.rayo.sanchez@es.ey.com>
2026-06-12 14:11:48 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -22,3 +22,24 @@ export default defineConfig({
     host: true,
   },
 })
+
+import { defineConfig } from 'vite';
+import fs from 'fs';
+import path from 'path';
+
+export default defineConfig({
+  build: {
+    outDir: 'dist'
+  },
+  plugins: [
+    {
+      name: 'copy-config',
+      closeBundle() {
+        fs.copyFileSync(
+          path.resolve(__dirname, 'staticwebapp.config.json'),
+          path.resolve(__dirname, 'dist/staticwebapp.config.json')
+        );
+      }
+    }
+  ]
+});
